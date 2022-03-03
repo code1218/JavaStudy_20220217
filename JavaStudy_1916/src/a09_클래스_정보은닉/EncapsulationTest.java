6f79a1c42499aeb9d9d0a9b7413ebdba6ae02db7
ITPS <ITPS@gmail.com>
2022-03-03 12:51:17 +0900

    클래스 캡슐화, 배열

--- JavaStudy_1916/src/a09_클래스_정보은닉/EncapsulationTest.java
+++ JavaStudy_1916/src/a09_클래스_정보은닉/EncapsulationTest.java
@@ -3,6 +3,8 @@ package a09_클래스_정보은닉;
 public class EncapsulationTest {
 	public static void main(String[] args) {
 		Encapsulation encapsulation = new Encapsulation("1234", "kakao");
+		
+		
 		encapsulation.showInfo();
 		
 		encapsulation.setUsername("junil1111");
