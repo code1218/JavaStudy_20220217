ee115af56e99c157723fae25ec79bdb0f8d03885
ITPS <ITPS@gmail.com>
2022-03-29 10:57:22 +0900

    java 마무리

--- JavaStudy_1916/src/a02_변수/Cat.java
+++ JavaStudy_1916/src/a02_변수/Cat.java
@@ -14,5 +14,14 @@ public class Cat {
 		System.out.println("( 0 )\"\"\"\\");
 		System.out.println("|\"^\"`    |");
 		System.out.println("||_/=\\\\__|");
+		
+		final int HOURLY_WAGE = 9160;
+		int monthlyPay = 0;
+		double a = 9160 * 8 * 3;
+		double b = 9160 * 8 * 5;
+		double c = (9160 + (9160 * 0.05)) * 8 * 3;
+		double d = (9160 + (9160 * 0.05)) * 8 * 5;
+		monthlyPay = ((int)((a + b + c + d)/10))*10;
+		System.out.println(monthlyPay);
 	}
 }
